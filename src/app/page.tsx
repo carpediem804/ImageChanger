7cb517af8afbc3feca270605313d25a40c4c9ef7
서비스개발팀/민태홍 <1187489@device.autoever.com>
2024-04-29 15:43:54 +0900

    [chore] design change `

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,11 +12,13 @@ export default function Home() {
   }
 
   return (
+    <div style={{display:"flex",width:"100vw",height:"100vh"}}>
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <ImageEditor
             canGenerateEdits={true}
             createEdit={createEdit}
           />
     </main>
+    </div>
   );
 }
