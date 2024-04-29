d926c157739659d49b7ead2def0c1be73dc55817
서비스개발팀/민태홍 <1187489@device.autoever.com>
2024-04-29 14:03:02 +0900

    [feat] image changer with open ai

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    experimental: {
+        serverActions: {
+            bodySizeLimit: '6mb' // 예시로 5MB로 설정
+            
+        }
+      }
+};
 
 export default nextConfig;
---
+++ src/app/api/images/edit/route.ts
@@ -0,0 +1,31 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { IncomingForm } from 'formidable';
+
+// 응답 데이터 타입 정의
+
+export async function POST(request: Request) {
+    console.log("in post #### ");
+    const apiKey = process.env.OPENAPI_KEY;
+    const formData = await request.formData();
+    
+    // process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
+    const res = await fetch("https://api.openai.com/v1/images/edits", {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${apiKey}`,
+      },
+      body: formData,
+    });
+  
+    const data = await res.json();
+    return Response.json(data);
+  }
+
+
+  
+export const config = {
+    api: {
+      bodyParser: false
+    },
+  };
